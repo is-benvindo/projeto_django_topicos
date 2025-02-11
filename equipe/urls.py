62f145dc5376069f83bb4b88a6f27414ad526d01
izzy <israelbenvindo2@gmail.com>
2025-02-11 18:47:17 -0300

    Remover rota de detalhes da equipe

--- equipe/urls.py
+++ equipe/urls.py
@@ -5,5 +5,4 @@ app_name = "equipe"
 
 urlpatterns = [
     path('relatorios/', views.listar_equipes, name='listar_equipes'),
-    path('detalhes/<int:id>/', views.detalhes_equipe, name='detalhes_equipe'),  # Para ver detalhes de uma equipe específica
 ]
